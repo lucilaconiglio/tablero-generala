5ebf27a9f087c0ed59778feac4d9b9128deaf2d1
Lucila Coniglio <lucilavictoria_2000@hotmail.com>
2021-01-29 14:56:36 -0300

    Cambiando el funcionamiento de mi array

--- script.js
+++ script.js
@@ -2,36 +2,83 @@ let jugadores = () => {
 
     let cantidadJugadoresPosibles = [2, 3, 4, 5, 6, 7];
     let cantidadJugadores = parseInt(prompt('Cantidad de 2 a 7 jugadores'));
+
+    
+    function Player(jugador){
+        this.jugador = jugador;
+    }
+
+    let jugador = []
     
     
     if (cantidadJugadores === 2) {
 
         cantidadJugadoresPosibles = cantidadJugadoresPosibles.slice(0,1);
         console.log(cantidadJugadoresPosibles);
+
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        console.log(jugador);
+
        
     } else if (cantidadJugadores === 3) {
 
         cantidadJugadoresPosibles = cantidadJugadoresPosibles.slice(0,2);
         console.log(cantidadJugadoresPosibles);
 
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        jugador.push(new Player('Jugador 3'));
+        console.log(jugador);
+
     } else if (cantidadJugadores === 4) {
 
         cantidadJugadoresPosibles = cantidadJugadoresPosibles.slice(0,3);
         console.log(cantidadJugadoresPosibles);
+
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        jugador.push(new Player('Jugador 3'));
+        jugador.push(new Player('Jugador 4'));
+        console.log(jugador);
 
     } else if (cantidadJugadores === 5) {
 
         cantidadJugadoresPosibles = cantidadJugadoresPosibles.slice(0,4);
         console.log(cantidadJugadoresPosibles);
 
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        jugador.push(new Player('Jugador 3'));
+        jugador.push(new Player('Jugador 4'));
+        jugador.push(new Player('Jugador 5'));
+        console.log(jugador);
+
     } else if (cantidadJugadores === 6) {
 
         cantidadJugadoresPosibles = cantidadJugadoresPosibles.slice(0,5);
         console.log(cantidadJugadoresPosibles);
 
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        jugador.push(new Player('Jugador 3'));
+        jugador.push(new Player('Jugador 4'));
+        jugador.push(new Player('Jugador 5'));
+        jugador.push(new Player('Jugador 6'));
+        console.log(jugador);
+
     } else /*cantidad de jugadores 7*/ {
 
         console.log(cantidadJugadoresPosibles);
+
+        jugador.push(new Player('Jugador 1'));
+        jugador.push(new Player('Jugador 2'));
+        jugador.push(new Player('Jugador 3'));
+        jugador.push(new Player('Jugador 4'));
+        jugador.push(new Player('Jugador 5'));
+        jugador.push(new Player('Jugador 6'));
+        jugador.push(new Player('Jugador 7'));
+        console.log(jugador);
     }
 
 }
@@ -42,6 +89,7 @@ console.log(jugadores());
 
 // Objeto con nombre del jugador y la cantidad de puntos que hizo en cada seccion del juego
 
+/*
 function Player(nombre, puntos, puntosTotales){
     this.nombre = nombre;
     this.puntos =  {
@@ -77,7 +125,7 @@ let jugador = prompt('Ingrese el nombre del jugador')
 let nombre = new Player(jugador);
 
 console.log(nombre);
-
+ */
 
 
 
